557c73abc58433978fe69e2510b517a1456a7a1c
Alexander2702 <anisimov_am@mail.ru>
2022-04-04 15:46:38 +0400

    решение

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,29 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      Car car1 = new Car("Mers", 600);
+      Car car2 = new Car("BMV", 7);
+      Car car3 = new Car("Volvo", 80);
+      Car car4 = new Car("Ferrari", 90);
+
+      user1.setUserCar(car1);
+      user2.setUserCar(car2);
+      user3.setUserCar(car3);
+      user4.setUserCar(car4);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
+      System.out.println("-------------------------");
+      System.out.println(userService.getUserByCar("Mers", 600));
+      System.out.println("-------------------------");
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +46,10 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " + user.getUserCar());
          System.out.println();
       }
+
 
       context.close();
    }
